386ba2e65e39e0e396191771f491b41e5eae5202
ManuelLopezReviriego <34214466+ManuelLopezReviriego@users.noreply.github.com>
2018-02-21 18:41:26 +0100

    Create perror_strerror.c

---
+++ C/Manejo de errores/perror_strerror.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <errno.h>
+/*
+FUNCIONES: perror, strerror
+EXPLICACION:
+    -perror(msg): Muestra un mensaje msg seguido de :, un espacio y la representación
+    en string de la variable global errno.
+
+    -strerror(errno): Representación en string de la variable errno.
+
+    Nota: C guarda el código de error en errno
+*/
+int main(int argvc, char ** argv) {
+    FILE* pf;
+    pf = fopen ("unexist.txt", "rb");
+    perror("Ha habido un error!");
+    printf("Codigo de error (errno): %d", errno);
+    printf("\n%s", strerror(errno));
+    return 0;
+}
